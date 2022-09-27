210bfa61953b39e29d7dd9885ec26b47816a4e9c
amekatze <setharie@gmail.com>
2022-09-27 16:22:28 +0900

    Add simple AI player

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 
 const tictactoe = (function(){
-    const Player = (symbol) => {
-        return {
-            symbol,
-            combination : [],
-        }
-    }    
 
     const winningCombinations = [
         [0, 1, 2], [3, 4, 5],
@@ -14,10 +8,36 @@ const tictactoe = (function(){
         [0, 4, 8], [2, 4, 6]
     ]
 
-    // PLAYER
-    const player1 = Player('x');
-    const player2 = Player('o');
+    
+    // PLAYER  
+    const Player = (symbol, type) => {
+        return {
+            symbol,
+            type,
+            combination : [],
+            makeMove(cell){
+                if (type == 'Human') {
+                cell.classList.add(symbol);
+                checkWin(cell)
+                }
+            }
+        }
+    }  
+    
+
+    
+    const player1 = Player('x', 'Human');
+    const player2 = Player('o', 'Human');
     let activePlayer;
+
+    function setPlayer(){
+        let setting;
+        if (player2.type == 'Computer') setting = 'Human';
+        else setting = 'Computer';
+        player2.type = setting;
+        playerSelect.innerText = setting;
+    }
+
 
     // DOM -- BOARD
     const winMessage = document.getElementById('win-message')
@@ -25,23 +45,24 @@ const tictactoe = (function(){
     const restartButton = document.getElementById('restart')
     const cells = document.querySelectorAll(".cell")
     let board = Array.from(cells)
-    restartButton.addEventListener("click", restartGame)
+    let playAble = [0,1,2,3,4,5,6,7,8]
+    restartButton.addEventListener('click', restartGame)
+    const playerSelect = document.getElementById('player2');
+    playerSelect.addEventListener('click', setPlayer)
 
     // GAME 
     function gameStart(){
         activePlayer = player2;
-        cells.forEach(cell => cell.addEventListener('click', playerMove, {once :true}))
+        cells.forEach(cell => cell.addEventListener('click', takeTurns, {once :true}))
     }
-
+    
     gameStart()
 
-    function playerMove(){
-        activePlayer = activePlayer == player1 ? player2 : player1;
-        this.classList.add(activePlayer.symbol)
-        checkWin(this)
+    function takeTurns(){
+        activePlayer = activePlayer == player1? player2 : player1;
+        activePlayer.makeMove(this)
+        if (player2.type == 'Computer') computer();
         }  
-
-
 
     function checkWin(i){
         activePlayer.combination.push(board.indexOf(i))
@@ -59,28 +80,22 @@ const tictactoe = (function(){
             cell.classList.remove('o')
             player1.combination = []
             player2.combination = []
+            playAble = [0,1,2,3,4,5,6,7,8]
             gameStart()
         })
     }
 
 
 
-
-
-    //function computer(){
-        // Ai Plays after Player
-        // Looks for Index that Player played first number,
-        // removes all arrays that need that index
-        // picks index that has most arrays  
-                //console.log(player1.combination)
-                //const minMax = winningCombinations.filter(comb => {
-                    //return player1.combination.every(num => {
-                        //return comb.includes(num)})
-                //})
-                //console.log(minMax)
-            //}
-
-
+    function computer(){
+        activePlayer = player2;
+        playAble = playAble.filter(cell => !player1.combination.includes(cell) && !player2.combination.includes(cell))
+        let num = Math.floor(Math.random() * playAble.length);
+        let cell = cells[playAble[num]];
+        cell.classList.add(activePlayer.symbol);
+        console.log(cell, playAble, num)
+        checkWin(cell)
+    }
 
 })();
 
